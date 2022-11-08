b5eebcd1485bcb4743515b7cbbc82f10aa62deff
AJ Holland <aholland95@gmail.com>
2022-11-08 16:12:06 -0600

    updated index.ejs and main.js

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+const deleteBtn = document.querySelectorAll('.fa-trash')
+const item = document.querySelectorAll('.item span')
+const itemCompleted = document.querySelectorAll('.item span.completed')
+
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
+})
+
+Array.from(item).forEach((element)=>{
+    element.addEventListener('click', markComplete)
+})
+
+Array.from(itemCompleted).forEach((element)=>{
+    element.addEventListener('click', markUnComplete)
+})
+
+async function deleteItem(){
+    const itemText = this.parentNode.childNode[1].innerText
+    
+}
